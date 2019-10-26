ca61a40830036cf0fa5340143c1600737d1d0cdb
NohRa0 <57031761+NohRa0@users.noreply.github.com>
2019-10-26 15:16:09 +0630

    Update fbi.py

--- fbi.py
+++ fbi.py
@@ -3,7 +3,7 @@
 import json , sys , hashlib , os , time , marshal, getpass
 ###################################################################
 '''
-     Facebook Information 
+     Fb Info
 '''
 ###################################################################
 #                             COLOR
@@ -66,11 +66,11 @@ def show_program():
                     %sINFORMATION%s
  ------------------------------------------------------
 
-    Author     Hak9
-    Name       Facebook Information
+    Hello      World
+    Name       Fb Info
     Version    Full Version
     Date       08/04/2019 
-    Jabber     xhak9x@jabber.de
+    create      NohRa
 
 * if you find any errors or problems , please contact
   author
